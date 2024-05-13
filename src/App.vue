0d87fe189b4487bb466faf6d660cb0d38e9afc04
ryampeguero <ryampeguero@gmail.com>
2024-05-13 15:42:18 +0200

    feat: added vue-router and imported bootstrap and fortawesome in general scss

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script>
 
+export default{
+
+}
 </script>
 
 <template>
-
+    <h1>ciao</h1>
+    <router-view></router-view>
 </template>
 
 <style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+<h2>sono qui</h2>
+</template>
+
+<style>
+</style>
