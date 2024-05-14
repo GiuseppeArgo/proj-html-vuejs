591ce58a795cf7afa121a8dbb6151c16878abf14
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-05-14 18:12:09 +0200

    add: card team

---
+++ src/components/AboutCard.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    props: {
+        cardName: String,
+        cardJob: String,
+        cardDescription: String,
+        cardLocation: Array,
+        cardImg: String,
+    },
+    methods: {
+        getImageFlag(imageName) {
+
+            return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="card mb-3 my-card">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img :src="getImageFlag(cardImg)" class="img-fluid rounded-start m-5 my-img" :alt="cardName">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body m-5">
+                    <h5 class="card-title">{{ cardName }}</h5>
+                    <p class="card-text"><small class="text-body-secondary">{{ cardJob }}</small></p>
+                    <p class="card-text">{{ cardDescription }}</p>
+                    <p v-for="item in cardLocation"> <i class="fa-regular fa-circle-right"></i> {{ item }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+
+i {
+    color: $text-green;
+}
+
+.my-img {
+    width: 60%;
+    transition: .3s ease-in-out;
+}
+
+:hover .my-img {
+    width: 65%;
+}
+
+.my-card {
+    border-top: solid 5px $text-green ;
+}
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,36 @@
 <script>
+import AboutCard from '../components/AboutCard.vue';
 export default {
-
+    components: {
+        AboutCard,
+    },
+    data() {
+        return {
+            team: [
+                {
+                    name: "Mike Hart",
+                    job: "Principle Instructor",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper.",
+                    location: ["San Diego", "Los Angeles", "Fresno", "San Francisco"],
+                    img: "mike-hart-about-page-400x504.jpg"
+                },
+                {
+                    name: "John Smith",
+                    job: "Course Instructor",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper.",
+                    location: ["New York", "New Jersey", "Washington D.C", "Virginia"],
+                    img: "john-smith-about-page-400x504.jpg"
+                },
+                {
+                    name: "Angela Hart",
+                    job: "Driving Instructor",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper. Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper.",
+                    location: ["New York", "South Carolina", "Washington D.C", "Florida"],
+                    img: "angela-hart-about-page-400x504.jpg"
+                },
+            ]
+        };
+    }
 }
 </script>
 
@@ -23,6 +53,19 @@ export default {
         </div>
         <div>
             <img src="../assets/img/about-page-mike-intro-fade.png" alt="Mike" />
+        </div>
+    </section>
+    <section class="ourteam">
+        <div class="d-flex container flex-column align-items-center">
+            <h2 class="m-5 fw-bold text-center">Our Team</h2>
+            <p class="m-3 fs-4 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>
+                Nunc ultrices auctor sapien id cursus. Aliquam maximus turpis in vehicula semper.</p>
+
+
+            <div v-for="item in team">
+                <AboutCard :cardName="item.name" :cardJob="item.job" :cardDescription="item.description"
+                    :cardLocation="item.location" :cardImg="item.img" />
+            </div>
         </div>
     </section>
 </template>
@@ -55,5 +98,10 @@ p {
 
 img {
     width: 80%;
+}
+
+.ourteam {
+    background-color: #F1F5F7;
+
 }
 </style>
